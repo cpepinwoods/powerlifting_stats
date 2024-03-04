1156981f04e59db605a631f8633c56bf7fec7b1f
cpepinwoods <charliepepinwoods@gmail.com>
2024-03-03 23:39:09 -0500

    Created some beautiful soup work that gets rows with individual lifters

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+response = requests.get('https://usapl.liftingdatabase.com/competitions-view?id=120525')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+tables = soup.find_all('table')
+tables = tables[1].contents
+# lifters[1] is the first row of the table, which contains the column headers
+# lifters[3] is all the lifters
+lifters = tables[3].contents
+lifters = "".join(str(x) for x in lifters)
+lifters = lifters.split('<td></td>')
+print(lifters[0])
